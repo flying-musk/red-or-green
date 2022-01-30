10c8c1f60833c951b639368c0b668aa1a7a3b0bb
Flying Musk <musk.flying@gmail.com>
2022-01-30 10:00:51 +0800

    [red-or-green] Init project and set up lint configuration.

---
+++ src/components/RedOrGreen.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="r-outer">Red Or Green</div>
+</template>
+
+<script>
+export default {
+  name: 'RedOrGreen',
+};
+</script>
+
+<style scoped lang="scss">
+.r-outer {
+  border: solid 1px #cc0000;
+}
+</style>
